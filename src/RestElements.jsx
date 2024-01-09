a179768401c796bcd563c343f41d1342c27dc80c
Johan Swan <jswannieman@gmail.com>
2024-01-09 15:49:58 +1300

    Update RestElements JSX Component

--- src/RestElements.jsx
+++ src/RestElements.jsx
@@ -1,31 +1,57 @@
 import React from "react";
 
-const ReactElement4 = React.createElement(
+const ReactElement3 = React.createElement(
   "a",
   { href: "https://google.com", target: "_blank", className: "link" },
   "GOOGLE"
 );
 
-const ReactElement5 = React.createElement(
+const ReactElement4 = React.createElement(
   "a",
   { href: "https://facebook.com", target: "_blank", className: "link" },
   "FACEBOOK"
 );
 
-const ReactElement6 = React.createElement(
+const ReactElement5 = React.createElement(
   "a",
   { href: "https://linkedin.com", target: "_blank", className: "link" },
   "LINKEDIN"
 );
 
-const ReactElement7 = React.createElement(
+const ReactElement6 = React.createElement(
   "a",
   { href: "https://github.com", target: "_blank", className: "link" },
   "GITHUB"
 );
 
+const ReactElement7 = React.createElement(
+  "a",
+  { href: "https://twitter.com", target: "_blank", className: "link" },
+  "TWITTER"
+);
+
+const ReactElement8 = React.createElement(
+  "a",
+  { href: "https://whatsapp.com", target: "_blank", className: "link" },
+  "WHATSAPP"
+);
+
+const ReactElement9 = React.createElement(
+  "a",
+  { href: "https://instagram.com", target: "_blank", className: "link" },
+  "INSTAGRAM"
+);
+
 const RestElements = () => {
-  return [ReactElement4, ReactElement5, ReactElement6, ReactElement7];
+  return [
+    ReactElement3,
+    ReactElement4,
+    ReactElement5,
+    ReactElement6,
+    ReactElement7,
+    ReactElement8,
+    ReactElement9,
+  ];
 };
 
 export default RestElements;
